33868fb9e074d026c174755b13ec7ebc52cb02a8
Minh <minhdotcom@gmail.com>
2019-03-27 16:39:16 +0700

    lan 1

--- untitled/myFunction.js
+++ untitled/myFunction.js
@@ -12,3 +12,20 @@ document.write('tong diem la '+tongdiem);
 document.write('<br/>');
 let diemtb = (diemly + diemsinh + diemhoa)/3;
 document.write ('diem trung binh la '+diemtb);
+document.write('<br/>');
+
+let inputdoC;
+inputdoC = prompt('nhap do C');
+let doC = parseInt(inputdoC);
+let doF = doC*9/5+32;
+document.write('do F la '+doF);
+document.write('<br/>');
+
+let inputbankinh;
+inputbankinh = prompt('nhap ban kinh');
+let bankinh = parseInt(inputbankinh);
+let chuvi = 2*math.PI*bankinh;
+document.write('chu vi la '+chuvi);
+document.write('<br/>');
+let dientich = math.PI*bankinh*bankinh;
+document.write('dien tich la '+dientich);
