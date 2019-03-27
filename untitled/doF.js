38bc0a320f234b8ebd1d5edbb1df9b2321b87fc3
Minh <minhdotcom@gmail.com>
2019-03-27 16:43:18 +0700

    lan 1

---
+++ untitled/doF.js
@@ -0,0 +1,6 @@
+let inputdoC;
+inputdoC = prompt('nhap do C');
+let doC = parseInt(inputdoC);
+let doF = doC*9/5+32;
+document.write('do F la '+doF);
+document.write('<br/>');
---
+++ untitled/hinhtron.js
@@ -0,0 +1,8 @@
+let inputbankinh;
+inputbankinh = prompt('nhap ban kinh');
+let bankinh = parseInt(inputbankinh);
+let chuvi = 2*math.PI*bankinh;
+document.write('chu vi la '+chuvi);
+document.write('<br/>');
+let dientich = math.PI*bankinh*bankinh;
+document.write('dien tich la '+dientich);
