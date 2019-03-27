dac507fab847efeaca63aabb62cf72540f050247
Minh <minhdotcom@gmail.com>
2019-03-27 16:47:16 +0700

    1

--- untitled/hinhtron.js
+++ untitled/hinhtron.js
@@ -1,8 +1,8 @@
 let inputbankinh;
 inputbankinh = prompt('nhap ban kinh');
 let bankinh = parseInt(inputbankinh);
-let chuvi = 2*m3.14*bankinh;
+let chuvi = 2*Math.PI*bankinh;
 document.write('chu vi la '+chuvi);
 document.write('<br/>');
-let dientich = math.PI*bankinh*bankinh;
+let dientich = Math.PI*bankinh*bankinh;
 document.write('dien tich la '+dientich);
