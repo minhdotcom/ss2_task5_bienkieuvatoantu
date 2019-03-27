b0be077c2b273c53d8e6b9360fef86360c3c3afd
Minh <minhdotcom@gmail.com>
2019-03-27 16:24:03 +0700

    lan 1

---
+++ untitled/myFunction.js
@@ -0,0 +1,13 @@
+let inputdiemly;
+let inputdiemsinh;
+let inputdiemhoa;
+inputdiemly = prompt('nhap diem ly');
+inputdiemsinh = prompt('nhap diem sinh');
+inputdiemhoa = prompt('nhap diem hoa');
+let diemly = parseInt(inputdiemly);
+let diemsinh = parseInt(inputdiemsinh);
+let diemhoa = parseInt(inputdiemhoa);
+let tongdiem = diemly + diemsinh + diemhoa;
+document.write('tonge diem la '+tongdiem);
+let diemtb = (diemly + diemsinh + diemhoa)/3;
+document.write ('diem trung binh la'+diemtb);
